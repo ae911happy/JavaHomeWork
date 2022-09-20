f1997a0fd5c110f1728392497871524bd7bb8438
ae911happy <ae911happy@163.com>
2022-09-20 08:23:11 +0800

    note

---
+++ src/NOte/Note.java
@@ -0,0 +1,30 @@
+package NOte;
+
+public class Note {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+
+    public String talk;
+    {
+        String info;
+        info="6666"+this.getName();
+        return info;
+    }
+}
